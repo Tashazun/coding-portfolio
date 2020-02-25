3e619d57a8f268b51b93315973982f2a01465343
Tasha Zuniga <tashazun@gmail.com>
2020-02-24 18:31:54 -0800

    adjusting structure and successful query to props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout/layout"
 
-const IndexPage = () => (
+const IndexPage = (props) => {
+  return (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {props.data.allCode.edges.map(edge => (
+      <div key={edge.node.id}>
+        <h2>{edge.node.title}</h2>
+      </div>
+    ))}
   </Layout>
-)
+  )
+};
+
+export const query = graphql`
+  {
+    allCode {
+      edges {
+        node {
+          id
+          image
+          link
+          summary
+          title
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage
