2d3d264f401cd61ac52e3ab95f6376a2257629a2
Tasha Zuniga <tashazun@gmail.com>
2020-02-27 21:55:13 -0800

    adding image config

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,8 +64,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-remote-images`,
+      options: {
+        nodeType: 'Design',
+        imagePath: 'img',
+      },
+    },
   ],
 }
